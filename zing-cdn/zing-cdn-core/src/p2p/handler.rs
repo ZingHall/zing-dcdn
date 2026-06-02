66d8a808466a6f47b3367752717c2c7eea2059d2
Jarek Lin <s410673074@gmail.com>
2026-06-02 15:41:43 +0800

    feat: add async data handler for inbound blob requests

--- zing-cdn/zing-cdn-core/src/p2p/handler.rs
+++ zing-cdn/zing-cdn-core/src/p2p/handler.rs
@@ -1,37 +1,31 @@
 use crate::cache::store::BlobStore;
-use crate::p2p::node::BlobResponse;
+use crate::p2p::protocol::BlobRequest;
+use crate::p2p::protocol::BlobResponse;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
 pub type BlobStoreHandle = Arc<RwLock<BlobStore>>;
 
-pub struct BlobRequestHandler {
-    store: BlobStoreHandle,
-}
+pub async fn handle_inbound_request(
+    store: &BlobStoreHandle,
+    request: BlobRequest,
+) -> BlobResponse {
+    let blob_id_hex = hex::encode(request.blob_id);
+    tracing::info!(blob_id = %blob_id_hex, "handling inbound blob request");
 
-impl BlobRequestHandler {
-    pub fn new(store: BlobStoreHandle) -> Self {
-        Self { store }
-    }
-
-    pub fn handle_request(&self, request: crate::p2p::node::BlobRequest) -> BlobResponse {
-        let blob_id_hex = hex::encode(request.blob_id);
-        tracing::info!(blob_id = %blob_id_hex, "received blob request from peer");
-
-        let store = self.store.blocking_read();
-        match store.get(&blob_id_hex) {
-            Ok(Some(data)) => {
-                tracing::info!(blob_id = %blob_id_hex, size = data.len(), "responding HAVE to peer");
-                BlobResponse::Have { size: data.len() as u64 }
-            }
-            Ok(None) => {
-                tracing::info!(blob_id = %blob_id_hex, "responding NOT_FOUND to peer");
-                BlobResponse::NotFound
-            }
-            Err(e) => {
-                tracing::error!(blob_id = %blob_id_hex, error = %e, "error reading blob from store");
-                BlobResponse::NotFound
-            }
+    let store = store.read().await;
+    match store.get(&blob_id_hex) {
+        Ok(Some(data)) => {
+            tracing::info!(blob_id = %blob_id_hex, size = data.len(), "responding HAVE");
+            BlobResponse::have(data)
+        }
+        Ok(None) => {
+            tracing::info!(blob_id = %blob_id_hex, "responding NOT_FOUND");
+            BlobResponse::not_found()
+        }
+        Err(e) => {
+            tracing::error!(blob_id = %blob_id_hex, error = %e, "store read error");
+            BlobResponse::not_found()
         }
     }
 }
